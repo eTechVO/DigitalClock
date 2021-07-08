645d64dc83e2ed5e7aa7df09a739a978407d32e7
eTechVO <contact@maesloic.fr>
2021-07-08 03:11:13 +0200

    [Add] Download button

--- files/scripts/settings.js
+++ files/scripts/settings.js
@@ -2,6 +2,7 @@ import { $, $a } from "./min.js";
 
 const toggleBtn = $(document, '.digital__settings__toggle')
 const actions = $(document, '.digital__settings__actions')
+const downloadBtn = $(document, '.download')
 
 window.onclick = (e) => {
     if (e.target !== toggleBtn && e.target.parentNode !== actions && actions.classList.contains('opened')) {
@@ -10,3 +11,4 @@ window.onclick = (e) => {
     }
 }
 toggleBtn.onclick = () => { actions.classList.toggle('opened') }
+downloadBtn.onclick = () => { window.open('https://github.com/eTechVO/DigitalClock/releases', '_blank') }
